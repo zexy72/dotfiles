c841aff3463fffbe4056734b3d8d482750447c9a
zexy72 <76520434+zexy72@users.noreply.github.com>
2021-01-16 19:50:07 +0530

    Create arch_basic.sh
    Basic Arch Linux Apps

---
+++ arch_basic.sh
@@ -0,0 +1,2 @@
+sudo pacman -Syu zsh zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k powerline powerline-fonts wget thunar thunar-archive-plugin thunar-volman thunar-media-tag-plugins tint2 xorg xorg-xinit xf86-video-amdgpu alsa-utils archlinux-wallpaper nitrogen alacritty 
+yay -S rofi-git polybar 
