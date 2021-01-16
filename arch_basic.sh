cec83ed320e108844da0308ce11230b1c2528490
zexy72 <76520434+zexy72@users.noreply.github.com>
2021-01-16 19:55:15 +0530

    Update arch_basic.sh

--- arch_basic.sh
+++ arch_basic.sh
@@ -1,2 +1,9 @@
-sudo pacman -Syu zsh zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k powerline powerline-fonts wget thunar thunar-archive-plugin thunar-volman thunar-media-tag-plugins tint2 xorg xorg-xinit xf86-video-amdgpu alsa-utils archlinux-wallpaper nitrogen alacritty 
-yay -S rofi-git polybar 
+#Basic Apps
+sudo pacman -Syu base-devel git zsh zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k powerline powerline-fonts wget thunar thunar-archive-plugin thunar-volman thunar-media-tag-plugins tint2 xorg xorg-xinit xf86-video-amdgpu alsa-utils archlinux-wallpaper nitrogen alacritty 
+#Installing Yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+#Basic Aur Apps
+yay -S rofi-git polybar breeze-default-cursor-theme
